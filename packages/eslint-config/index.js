eb064e4821f51c29ed12d6c36b6899b9b6c8d457
Jarrod Seccombe <Jarrod.Seccombe@icloud.com>
2023-08-03 21:22:24 -0400

    Add eslint, prettier, and tsconfig configurations

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+const rules = {
+  "comma-dangle": "off",
+  "object-curly-spacing": "off",
+  "operator-linebreak": "off",
+  "no-mixed-spaces-and-tabs": "off",
+  "@typescript-eslint/comma-dangle": "off",
+  "@typescript-eslint/indent": "off",
+  "@typescript-eslint/quotes": "off",
+  "@typescript-eslint/semi": "off",
+  "@typescript-eslint/object-curly-spacing": "off",
+  "no-unused-vars": "off",
+  "no-warning-comments": "off",
+  "simple-import-sort/imports": "error",
+  "simple-import-sort/exports": "error",
+  "import/first": "error",
+  "import/newline-after-import": "error",
+  "import/no-duplicates": "error",
+};
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  env: {
+    node: true,
+  },
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "import",
+    "simple-import-sort",
+    "prettier",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "xo",
+    "xo-typescript",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  rules,
+  overrides: [
+    {
+      files: ["**/*/*.{test,spec}.ts"],
+      rules: {
+        ...rules,
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/unbound-method": "off",
+      },
+    },
+  ],
+};
