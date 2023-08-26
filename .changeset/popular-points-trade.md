cafb3ab5f27e4b93c851b9e805370dcebc24459f
Jarrod Seccombe <Jarrod.Seccombe@icloud.com>
2023-08-26 11:41:13 -0400

    feat(prettier): switch default indentation from 4 spaces to 2

---
+++ .changeset/popular-points-trade.md
@@ -0,0 +1,5 @@
+---
+"@jarsec/prettier-config": minor
+---
+
+Switch from 4 space to 2 space indentation
--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-    singleQuote: true,
-    semi: true,
-    arrowParens: 'avoid',
-    trailingComma: 'none',
-    bracketSpacing: true,
-    endOfLine: 'auto',
-    printWidth: 120,
-    tabWidth: 4,
-    useTabs: false
+  singleQuote: true,
+  semi: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false
 };
