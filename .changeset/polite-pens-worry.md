bc38fb2ba3ba92b70e98418637783e0fc747488e
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-10-24 22:40:47 -0400

    feat(eslint): update eslint to v9

---
+++ .changeset/polite-pens-worry.md
@@ -0,0 +1,10 @@
+---
+'@jarsec/eslint-config': major
+---
+
+Update to ESLint v9
+
+- Bump ESLint dependencies to support the v9 migration.
+- Update config file to flat config format.
+- Drop eslint-plugin-import until they can get their lives together.
+- Simplify @stylistic configuration.
---
+++ packages/eslint-config/index.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import nodePlugin from 'eslint-plugin-n';
+import perfectionist from 'eslint-plugin-perfectionist';
+import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  nodePlugin.configs['flat/recommended'],
+  unicorn.configs['flat/recommended'],
+  perfectionist.configs['recommended-natural'],
+  stylistic.configs.customize({
+    arrowParens: false,
+    blockSpacing: true,
+    braceStyle: '1tbs',
+    commaDangle: 'always-multiline',
+    flat: true,
+    indent: 2,
+    jsx: true,
+    pluginName: '@stylistic',
+    quoteProps: 'consistent-as-needed',
+    quotes: 'single',
+    semi: true,
+  }),
+  {
+    rules: {
+      'multiline-comment-style': ['error', 'separate-lines'],
+      'n/no-missing-import': 'off',
+      'n/no-process-exit': 'off',
+      'unicorn/no-process-exit': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
+);
