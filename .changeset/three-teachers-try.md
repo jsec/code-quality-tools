5a293fc994da27f1210aae0373413a3b728f692d
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-02-28 21:02:26 -0500

    feat(eslint): update deprecated shared configurations

---
+++ .changeset/three-teachers-try.md
@@ -0,0 +1,6 @@
+---
+'@jarsec/eslint-config': minor
+---
+
+- Update deprecated unicorn config
+- Remove deprecated `flat` option from @stylistic factory function
--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,43 +1,42 @@
 // @ts-check
 
-import eslint from '@eslint/js'
-import stylistic from '@stylistic/eslint-plugin'
-import nodePlugin from 'eslint-plugin-n'
-import perfectionist from 'eslint-plugin-perfectionist'
-import unicorn from 'eslint-plugin-unicorn'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import nodePlugin from 'eslint-plugin-n';
+import perfectionist from 'eslint-plugin-perfectionist';
+import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  nodePlugin.configs['flat/recommended'],
-  unicorn.configs['flat/recommended'],
-  perfectionist.configs['recommended-natural'],
-  stylistic.configs.customize({
-    arrowParens: false,
-    blockSpacing: true,
-    braceStyle: '1tbs',
-    commaDangle: 'always-multiline',
-    flat: true,
-    indent: 4,
-    jsx: true,
-    pluginName: '@stylistic',
-    quoteProps: 'consistent-as-needed',
-    quotes: 'single',
-    semi: true,
-  }),
-  {
-    rules: {
-      '@typescript-eslint/consistent-type-definitions': [
-        'error',
-        'type',
-      ],
-      'multiline-comment-style': ['error', 'starred-block'],
-      'n/no-missing-import': 'off',
-      'n/no-process-exit': 'off',
-      'unicorn/no-process-exit': 'off',
-      'unicorn/prevent-abbreviations': 'off',
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    nodePlugin.configs['flat/recommended'],
+    unicorn.configs.recommended,
+    perfectionist.configs['recommended-natural'],
+    stylistic.configs.customize({
+        arrowParens: false,
+        blockSpacing: true,
+        braceStyle: '1tbs',
+        commaDangle: 'always-multiline',
+        indent: 4,
+        jsx: true,
+        pluginName: '@stylistic',
+        quoteProps: 'consistent-as-needed',
+        quotes: 'single',
+        semi: true,
+    }),
+    {
+        rules: {
+            '@typescript-eslint/consistent-type-definitions': [
+                'error',
+                'type',
+            ],
+            'multiline-comment-style': ['error', 'starred-block'],
+            'n/no-missing-import': 'off',
+            'n/no-process-exit': 'off',
+            'unicorn/no-process-exit': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+        },
     },
-  },
-)
+);
