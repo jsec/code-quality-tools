8b8f3767359b60f0ac249cb033f34be9ad8f0011
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-07-13 19:14:46 -0400

    feat(eslint): Add react config

---
+++ packages/eslint-config/configs/react.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import baseConfig from './base.js';
+
+export default tseslint.config(
+    ...baseConfig,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    reactPlugin.configs.flat.recommended,
+    {
+        languageOptions: {
+            ...reactPlugin.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.serviceworker,
+                ...globals.browser,
+            },
+        },
+    },
+    {
+        plugins: {
+            'react-hooks': hooksPlugin,
+        },
+        rules: {
+            ...hooksPlugin.configs.recommended.rules,
+            // React scope no longer necessary with new JSX transform.
+            'react/react-in-jsx-scope': 'off',
+        },
+        settings: { react: { version: 'detect' } },
+    },
+);
